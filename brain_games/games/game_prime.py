ee41c38b5ccbf94b2721126d538d11091a31e2d0
Sergey R <topgoods687@gmail.com>
2023-02-26 17:47:22 +0300

    add brain-prime

---
+++ brain_games/games/game_prime.py
@@ -0,0 +1,24 @@
+from random import randint
+from brain_games import games_logic
+
+def is_prime(number):
+    if number == 1:
+        return 'yes'
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                return 'no'
+        return 'yes'
+
+
+def main(player_name):
+    try_number = 1
+    
+    while try_number <= 3:
+        number = randint(0, 100)
+        answer = is_prime(number)
+        if games_logic.main(answer, number, player_name):
+            try_number += 1
+        else:
+            return None    
+    print(f'Congratulations, {player_name}!')
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+from brain_games import greetings
+from brain_games.games import game_prime
+
+
+def main():
+    name = greetings.welcome_user()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    game_prime.main(name)
+
+
+if __name__ == '__main__':
+        main()
