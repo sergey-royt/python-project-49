61404d8fb62e266bdeadc4c7187914c1606184f0
Sergey R <topgoods687@gmail.com>
2023-03-05 15:28:28 +0300

    fixing brain-progression range error

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -1,12 +1,14 @@
 from random import randint
 
 
+PROGRESSION_LENGTH = 9
+
+
 def rules():
     return 'What number is missing in the progression?'
 
 
 def get_progression():
-    PROGRESSION_LENGTH = 9
     INITIAL_ITEM_RANGE_BOTTOM = 0
     INITIAL_ITEM_RANGE_TOP = 10
     COMMON_DIFFERENCE_RANGE_BOTTOM = 2
@@ -28,11 +30,8 @@ def progression_to_string(progression):
 
 
 def game():
-    HIDDEN_NUMBER_RANGE_BOTTOM = 0
-    HIDDEN_NUMBER_RANGE_TOP = 10
-
     progression = get_progression()
-    hidden_index = randint(HIDDEN_NUMBER_RANGE_BOTTOM, HIDDEN_NUMBER_RANGE_TOP)
+    hidden_index = randint(0, PROGRESSION_LENGTH)
     hidden_number = str(progression[hidden_index])
     progression[hidden_index] = '..'
     question = progression_to_string(progression)
